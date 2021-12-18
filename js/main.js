cc15b4fb9c1eb3a0a7e52065c36f236ab231469a
Awad2002 <awadamr33@gmail.com>
2021-12-18 09:53:52 +0200

    sdasdsadsad

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ $('.city .city-item').click(function() {
     $(this).find('i').toggleClass('fa-angle-up');
     $(this).find('i').toggleClass('fa-angle-down');
 });
+
+$(".area-item").click(function(e) {
+    e.preventDefault();
+    window.location = "time.html";
+});
+
+$(".categorie-item").click(function(e) {
+    e.preventDefault();
+    window.location = "categorie.html";
+});
+
+$(".product-item").click(function(e) {
+    e.preventDefault();
+    window.location = "product.html";
+});
